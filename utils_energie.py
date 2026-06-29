7ad437373834b6a655e970d543eef2725f778a0f
Martin Pouliquen <martin.pouliquen@etu.minesparis.psl.eu>
2026-06-29 15:34:44 +0200

    modif

--- utils_energie.py
+++ utils_energie.py
@@ -1,9 +1,7 @@
-import argparse
 from pathlib import Path
 import numpy as np
 import pandas as pd
 import matplotlib
-#matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
